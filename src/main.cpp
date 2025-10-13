e47281203e03dc8317d832c726adbb174ef73eaa
zheyu96 <zheyu@gmail.com>
2025-10-14 01:50:32 +0800

    640 modify

--- src/main.cpp
+++ src/main.cpp
@@ -205,7 +205,7 @@ int main(){
 
                     double A = 0.25, B = 0.75, tao = input_parameter["tao"], T = 0.04, n = 2;
 
-                    Graph graph(filename, time_limit, swap_prob, avg_memory, min_fidelity, max_fidelity, fidelity_threshold, A, B, n, T, tao, entangle_prob);
+                    Graph graph(filename, time_limit, swap_prob, avg_memory, min_fidelity, max_fidelity, fidelity_threshold, A, B, n, T, tao);
 
                     ofs << "--------------- in round " << r << " -------------" <<endl;
                     vector<pair<int, int>> requests;
