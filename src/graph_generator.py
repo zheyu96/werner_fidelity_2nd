3d676180d429f73484426cec5bf072956baae421
zheyu96 <zheyu@gmail.com>
2025-10-18 22:34:58 +0800

    adjust param

--- src/graph_generator.py
+++ src/graph_generator.py
@@ -123,7 +123,7 @@ with open(path, 'w') as f:
             # F = random.random()*(max_fidelity-min_fidelity) + min_fidelity  # fidelity
             # ratio_list=[0.98,0.7]
             # ratio=numpy.random.choice(ratio_list,p=[0.3,0.7])
-            ratio = numpy.random.normal(1, 0.2)
+            ratio = numpy.random.normal(1, 0.15)
             dif = abs(1 - ratio)
             ratio = 1 - dif
             if ratio > 1:
