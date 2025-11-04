b5f5f389c0bfeaed28d3e0fe51cab963d8f4a697
zheyu96 <zheyu@gmail.com>
2025-11-05 00:19:44 +0800

    fixed_obj&request

--- src/main.cpp
+++ src/main.cpp
@@ -111,7 +111,7 @@ int main(){
 
     map<string, double> default_setting;
     default_setting["num_nodes"] = 100;
-    default_setting["request_cnt"] = 130;
+    default_setting["request_cnt"] = 90;
     default_setting["entangle_lambda"] = 0.045;
     default_setting["time_limit"] = 13;
     default_setting["avg_memory"] = 10; // 16
@@ -127,16 +127,16 @@ int main(){
     default_setting["bucket_eps"]=0.01;
     default_setting["time_eta"]=0.001;
     map<string, vector<double>> change_parameter;
-    change_parameter["request_cnt"] = {110,130,150,170,190,210};
+    change_parameter["request_cnt"] = {30,50,70,90,110,130,150,170,190,210};
     change_parameter["num_nodes"] = {40, 70, 100, 130, 160};
     change_parameter["min_fidelity"] = {0.6, 0.7, 0.8, 0.9, 0.95};
-    change_parameter["avg_memory"] = {6, 8, 10, 12, 14};
+    change_parameter["avg_memory"] = {4,6, 8, 10, 12, 14,16};
     // change_parameter["tao"] = {0.3, 0.4, 0.5, 0.6, 0.7};
-    change_parameter["tao"] = {0.0015, 0.00175, 0.002,0.00225,0.0025};
+    change_parameter["tao"] = {0.0015, 0.00175, 0.002,0.00225,0.0025,0.00275,0.003};
     change_parameter["path_length"] = {3, 6, 9, 12, 15};
     change_parameter["swap_prob"] = {0.6, 0.7, 0.8, 0.9,0.95};
     change_parameter["fidelity_threshold"] = {0.7, 0.75, 0.8, 0.85, 0.9};
-    change_parameter["time_limit"] = {7, 9, 11, 13, 15};
+    change_parameter["time_limit"] = {3,5,7, 9, 11, 13, 15};
     change_parameter["entangle_lambda"] = {0.0125, 0.025, 0.035, 0.045, 0.055, 0.065};
     change_parameter["entangle_time"] = {0.0001, 0.00025, 0.0004, 0.00055, 0.0007,0.00085,0.001};
     change_parameter["entangle_prob"] = {0.0001, 0.001, 0.01, 0.1, 1};
