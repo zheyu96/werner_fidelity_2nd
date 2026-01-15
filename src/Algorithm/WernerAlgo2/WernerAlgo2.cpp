266ee9892e596325910def79b278a5262ebe4ead
zheyu96 <zheyu@gmail.com>
2026-01-15 20:02:24 +0800

    add ent_time adj

--- src/Algorithm/WernerAlgo2/WernerAlgo2.cpp
+++ src/Algorithm/WernerAlgo2/WernerAlgo2.cpp
@@ -224,7 +224,7 @@ Shape_vector WernerAlgo2::backtrack_shape(ZLabel leaf,const vector<int>& path){
         double w=graph.get_link_werner(left_id,right_id);
         double new_w=1.0L-(1.0L-graph.get_link_werner(left_id,right_id))/(leaf.ent_time[1]-leaf.ent_time[0]);
         double new_fid= (3.0*new_w +1.0)/4.0;
-        if(leaf.ent_time[1]-leaf.ent_time[0]>=2)
+        if(leaf.ent_time[1]-leaf.ent_time[0]<=1)
             cout<<left_id<<" "<<right_id<<" "<<leaf.ent_time[0]<<" "<<leaf.ent_time[1]<<" "<<fid<<" "<<new_fid<<"\n";
         else
             cout<<"\033[1;31m"<< left_id << " " << right_id << " " << leaf.ent_time[0] << " " << leaf.ent_time[1] << " " << fid << " " << new_fid << "\033[0m" << "\n";
