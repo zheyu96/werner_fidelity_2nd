66baa7632a5f569d7b851c074da1a1f6f158e54e
zheyu96 <zheyu@gmail.com>
2026-01-16 13:15:16 +0800

    fix time calculate

--- src/Algorithm/WernerAlgo2/WernerAlgo2.cpp
+++ src/Algorithm/WernerAlgo2/WernerAlgo2.cpp
@@ -423,10 +423,10 @@ void WernerAlgo2::run() {
             }
 
             if(request_index == -1 || used[request_index]) continue;
-            if(graph.check_resource(shape,true,true)) {
+            if(graph.check_resource(shape,true,false)) {
                 used[request_index] = true;
                 // cerr << "[MyAlgo1] " << P.first << " " << P.second.size() << endl;
-                graph.reserve_shape(shape,true);
+                graph.reserve_shape(shape,false);
                 finished.push_back(request_index);
             }
         }
