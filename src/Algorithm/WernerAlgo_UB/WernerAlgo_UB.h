333d8ebdb6409c79e58098b1336420cd051dde5b
zheyu96 <zheyu@gmail.com>
2026-01-18 00:41:53 +0800

    add ZFA UB

--- src/Algorithm/WernerAlgo_UB/WernerAlgo_UB.h
+++ src/Algorithm/WernerAlgo_UB/WernerAlgo_UB.h
@@ -19,7 +19,7 @@ using namespace std;
 class WernerAlgo_UB : public AlgorithmBase {
 public:
     // 為了保持精度，特定計算使用 long double，但介面仍維持 double
-    #define ld long double
+    #define double long double
     
     WernerAlgo_UB(Graph graph,
                vector<pair<int,int>> requests,
@@ -32,20 +32,20 @@ private:
     enum class Op : unsigned char { LEAF = 0, CONT = 1, MERGE = 2 };
     class ZLabel {
     public:
-        ld B = 1000.0;
-        ld Z = 1000.0;   // 成本 / 目標
+        double B = 1000.0;
+        double Z = 1000.0;   // 成本 / 目標
         int a = -1, b = -1, t = -1, k = -1; 
         int left_id = -1, right_id = -1, parent_id = -1;
         Op op = Op::LEAF;
         vector<int> ent_time; // 用於儲存 entanglement 產生的時間區段
         
         ZLabel(){}
-        ZLabel(ld _B, ld _Z, Op _op, int _a, int _b, int _t, int _k, int pid = -1, int lid = -1, int rid = -1)
+        ZLabel(double _B, double _Z, Op _op, int _a, int _b, int _t, int _k, int pid = -1, int lid = -1, int rid = -1)
         : B(_B), Z(_Z), op(_op), a(_a), b(_b), t(_t), k(_k), parent_id(pid), left_id(lid), right_id(rid) {}
     };
 
     struct DPParam{
-        ld eps_bucket, Zhat, Zmin, eta, T;
+        double eps_bucket, Zhat, Zmin, eta, T;
         int tau_max;
     } dpp;
 
