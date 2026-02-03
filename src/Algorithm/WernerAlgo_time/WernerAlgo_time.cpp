c2dc807b2c8f2a77534928e3d7d5d4027fb3980c
zheyu96 <zheyu@gmail.com>
2026-02-04 00:28:30 +0800

    add zfa2 runtime

--- src/Algorithm/WernerAlgo_time/WernerAlgo_time.cpp
+++ src/Algorithm/WernerAlgo_time/WernerAlgo_time.cpp
@@ -160,7 +160,6 @@ void WernerAlgo_time::run_dp_in_t(const Path& path, const DPParam& dpp,int t) {
             cerr<<"\033[93mDP_table[" << t << "][" << a << "][" << b << "]\033[0m - "
      << "before: \033[92m" << cand.size() << "\033[0m";
             bucket_by_Z(cand);
-            total_after += cand.size();
             cerr<<", after: \033[92m" << cand.size() << "\033[0m\n";
             DP_table[t][a][b]=cand;
         }
